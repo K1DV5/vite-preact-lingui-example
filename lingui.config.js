b58530479bc90d218520fad0af2794965428c415
Timofei Iatsenko <ty@time.sc>
2023-04-06 15:02:45 +0200

    use v4

--- lingui.config.js
+++ lingui.config.js
@@ -1,7 +1,10 @@
-{
+/** @type {import('@lingui/conf').LinguiConfig} */
+const config = {
   "locales": ["en", "am"],
   "catalogs": [{
     "path": "src/locales/{locale}",
     "include": ["src"]
   }],
 }
+
+export default config;
